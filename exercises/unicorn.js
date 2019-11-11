e6ba3c3a6f438e8d69ff6bbf889a281ac4b36fd4
Scott Payton <spayton.c@gmail.com>
2019-11-10 23:26:23 -0700

    finish unicorn critter

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,9 +1,18 @@
 class Unicorn {
   // Think of a constructor as being similar to initialize in Ruby
   // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color = "white") { //default color parameter
+    this.name = name; //this unicorn is instantiated with a name
+    this.color = color;
   }
-}
+    isWhite(){
+      return this.color === "white"
+    }
+
+    says(words){
+      return "**;* "+words+" *;**"
+    }
+}//end class
 
 
 // Notice here we are exporting our class.
